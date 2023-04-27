0f283544f7c73ea2979eb2447614cd1ecc6628b6
titouanlebocq <lebocq.titouan@gmail.com>
2023-04-27 12:25:24 +0200

    Fix display with gnuplot

--- sin_cos/sincos.c
+++ sin_cos/sincos.c
@@ -8,18 +8,20 @@
 
 #include "sincos.h"
 
+#define SAMPLING_RATE 1000
+
 double data = 0;
-int increment_value = 1;
+double increment_value = .01;
 
 void sin_signal_handler(int sig) {
-    data = fmod(data + increment_value, 360);
+    data += increment_value;
     double processed_data = sin(data);
     printf("sin(%f) = %f\n", data, processed_data);
     write_data_to_file(SIN_DATA_FILE, data, processed_data);
 }
 
 void cos_signal_handler(int sig) {
-    data = fmod(data + increment_value, 360);
+    data += increment_value;
     double processed_data = cos(data);
     printf("cos(%f) = %f\n", data, processed_data);
     write_data_to_file(COS_DATA_FILE, data, processed_data);
@@ -52,33 +54,27 @@ void child_process_routine(int type) {
 }
 
 void display_routine() {
-    int pid;
-
-    pid = fork();
-    if (pid == 0) {
-        if (execlp("gnuplot", "gnuplot", "-persist", COS_COMMAND_FILE, NULL) != 0) {
-            perror("execv");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
+    pid_t pid_sin = fork();
+    if (pid_sin == 0) {
+        execlp("gnuplot", "gnuplot", "-persist", SIN_COMMAND_FILE, NULL);
+        exit(EXIT_FAILURE);
     }
-
-    pid = fork();
-    if (pid == 0) {
-        if (execlp("gnuplot", "gnuplot", "-persist", SIN_COMMAND_FILE, NULL) != 0) {
-            perror("execv");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
+    pid_t pid_cos = fork();
+    if (pid_cos == 0) {
+        execlp("gnuplot", "gnuplot", "-persist", COS_COMMAND_FILE, NULL);
+        exit(EXIT_FAILURE);
     }
-
-    wait(NULL);
+    int status;
+    waitpid(pid_sin, &status, 0);
+    printf("status: %d\n", status);
+    waitpid(pid_cos, &status, 0);
+    printf("status: %d\n", status);
 }
 
 void parent_routine(int pid_sin, int pid_cos) {
     printf("[parent] started\n");
     // simple for of 10 iterations instead of infinite loop asked in the subject
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < SAMPLING_RATE; i++) {
         kill(pid_sin, SIGALRM);
         kill(pid_cos, SIGALRM);
         usleep(100); // used to display in terminal
